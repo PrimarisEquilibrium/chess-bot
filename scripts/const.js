1185ea77392d975b69c08d8158c3c6636c9857d5
Kevin <incorrectclock1@gmail.com>
2024-03-10 19:59:36 -0400

    Cleaned up and documented script.js and seperated constants into const.js

---
+++ scripts/const.js
@@ -0,0 +1,19 @@
+// Variable names for piece ranks
+const K = "K"
+const Q = "Q"
+const R = "R"
+const B = "B"
+const N = "N"
+const P = "P"
+
+// Variable names for piece colors
+const b = "B"
+const w = "W"
+
+// [row, col, rank, color]
+export const initialLayout = [
+    [0, 0, R, b], [0, 1, N, b], [0, 2, B, b], [0, 3, Q, b], [0, 4, K, b], [0, 5, B, b], [0, 6, N, b], [0, 7, R, b],
+    [1, 0, P, b], [1, 1, P, b], [1, 2, P, b], [1, 3, P, b], [1, 4, P, b], [1, 5, P, b], [1, 6, P, b], [1, 7, P, b],
+    [6, 0, P, w], [6, 1, P, w], [6, 2, P, w], [6, 3, P, w], [6, 4, P, w], [6, 5, P, w], [6, 6, P, w], [6, 7, P, w],
+    [7, 0, R, w], [7, 1, N, w], [7, 2, B, w], [7, 3, Q, w], [7, 4, K, w], [7, 5, B, w], [7, 6, N, w], [7, 7, R, w]
+]
--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,9 @@
+import { initialLayout } from "./const.js";
+
 document.addEventListener("DOMContentLoaded", () => {
     generateBoard()
     generatePieces()
 })
-
-// Variable names for piece ranks
-const K = "K"
-const Q = "Q"
-const R = "R"
-const B = "B"
-const N = "N"
-const P = "P"
-
-// Variable names for piece colors
-const b = "B"
-const w = "W"
-
-const initialLayout = [
-    [0, 0, R, b], [0, 1, N, b], [0, 2, B, b], [0, 3, Q, b], [0, 4, K, b], [0, 5, B, b], [0, 6, N, b], [0, 7, R, b],
-    [1, 0, P, b], [1, 1, P, b], [1, 2, P, b], [1, 3, P, b], [1, 4, P, b], [1, 5, P, b], [1, 6, P, b], [1, 7, P, b],
-    [6, 0, P, w], [6, 1, P, w], [6, 2, P, w], [6, 3, P, w], [6, 4, P, w], [6, 5, P, w], [6, 6, P, w], [6, 7, P, w],
-    [7, 0, R, w], [7, 1, N, w], [7, 2, B, w], [7, 3, Q, w], [7, 4, K, w], [7, 5, B, w], [7, 6, N, w], [7, 7, R, w]
-]
 
 /**
  * Creates a chess board DOM element and appends it to the chess-container 
@@ -54,17 +37,28 @@ function generateBoard() {
     }
 }
 
+/**
+ * Modifies the current chessboard and adds the starting layout
+*/
 function generatePieces() {
     for (let piece of initialLayout) {
-        [row, col, rank, color] = piece
+        let [row, col, rank, color] = piece
         let square = findSquare(row, col)
         let pieceImage = document.createElement("img")
+
+        // Uses rank and color shortnames to quickly access images
         pieceImage.src = `./images/${color}${rank}.svg`
         pieceImage.classList.add("chess-icon")
         square.appendChild(pieceImage)
     }
 }
 
+/**
+ * Returns the chess square DOM element given the row and col numbers
+ * @param {Int} row 
+ * @param {Int} col 
+ * @returns Chess square DOM element
+*/
 function findSquare(row, col) {
     let squares = document.querySelectorAll(".chess-square");
     for (let i = 0; i < squares.length; i++) {
