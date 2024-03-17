665baba1b4983b26267a9a725cf970b5121282fc
Kevin <incorrectclock1@gmail.com>
2024-03-17 18:18:06 -0400

    displayMoves takes piece as an argument rather than event

--- scripts/board.js
+++ scripts/board.js
@@ -13,6 +13,166 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 /**
+ * Allows data/elements to be dropped in other elements
+ * @param {Event} ev 
+ */
+function allowDrop(ev) {
+    ev.preventDefault()
+}
+
+
+/**
+ * Stores the dragged piece position in dataTransfer (storage method for drag and drop API)
+ * @param {Event} ev
+ */
+function drag(ev) {
+    let piece = ev.target
+
+    // The parent of the dragged chess icon is the chess square
+    let square = piece.parentNode
+    let row = square.dataset.row
+    let col = square.dataset.col
+
+    // Shows move guidance on drag of piece
+    displayMoves(piece)
+    
+    // Stores the position in a string where first digit is row and second digit is col
+    ev.dataTransfer.setData("pos", `${row}${col}`)
+}
+
+
+let prevPieces = []
+/**
+ * Modifies the chess board to show all available moves for a given piece
+ * @param {Node} piece 
+ */
+function displayMoves(piece) {
+    if (currentTurn === piece.dataset.color) {
+        let possibleMoves = getPossibleMoves(piece)
+        possibleMoves.forEach(
+            pos => findSquare(pos[0], pos[1]).classList.add("active-square")
+        )
+        prevPieces = possibleMoves
+    }
+}
+
+
+/**
+ * Clears all available moves for a given piece
+ */
+function clearMoves() {
+    prevPieces.forEach(
+        pos => findSquare(pos[0], pos[1]).classList.remove("active-square")
+    )
+}
+
+
+/**
+ * Retrieves the dragged chess piece and its position
+ * @param {Event} ev
+ * @returns {Array} [piece, [row, col]]
+ */
+function getDraggedPiece(ev) {
+    // Get the piece that was dragged
+    let [row, col] = ev.dataTransfer.getData("pos").split("")
+    let square = findSquare(parseInt(row), parseInt(col))
+    let piece = square.firstChild
+    return piece
+}
+
+
+/**
+ * Returns true if the move is valid; otherwise false. Valid moves are:
+ *  - Correct turn
+ *  - In possible moveset
+ *  - Inbounds
+ * @param {Node} piece
+ * @param {Node} droppedSquare
+ * @param {Array} currentPos
+ * @returns {Boolean} 
+ */
+function isValidMove(piece, droppedSquare) {
+    function deepIncludes(array, searchElement) {
+        for (const element of searchElement) {
+            if (array.length !== element.length) { 
+                return false 
+            }
+
+            let same = true
+            for (let i = 0; i < element.length; i++) {
+                if (!(array[i] === element[i])) {
+                    same = false
+                }
+            }
+            if (same) {
+                return true
+            }
+        }
+
+        return false
+    }
+
+    // Check if the piece being moved matches the current type
+    if (piece.dataset.color !== currentTurn) { return false }
+
+    let pos = getPos(droppedSquare)
+    console.log(pos, getPossibleMoves(piece))
+    if (!deepIncludes(pos, getPossibleMoves(piece))) { return false }
+
+    let hasMoved = piece.dataset.notMoved === "true" ? true : false
+    console.log(hasMoved)
+    if (piece.dataset.rank === "P") {
+        if (hasMoved) {
+            piece.dataset.notMoved = false
+        }
+    }
+
+    return true
+
+}
+
+
+/**
+ * When the dragged data is dropped, the chess piece element is moved to the according square
+ * @param {Event} ev
+ */
+function drop(ev) {
+    // Prevent default behavior of open as link on drop
+    ev.preventDefault()
+
+    // Clears piece move guidance
+    clearMoves()
+
+    // Get the square element dropped onto
+    let square = ev.target
+
+    // Prevents square from being the chess piece element
+    // (doesn't have row and col) dataset info
+    if (square.tagName === "IMG") {
+        square = ev.target.parentNode
+    }
+
+    // Get the piece that was dragged
+    let piece = getDraggedPiece(ev)
+
+    // Returns true if valid move, otherwise false
+    if (!isValidMove(piece, square)) { return }
+
+    // If there is a piece on the position, capture it
+    let opponentPiece = square.firstChild
+    if (opponentPiece) {
+        square.removeChild(opponentPiece)
+    }
+
+    // Place it on the new chess square
+    square.appendChild(piece)
+
+    // Switch turns
+    currentTurn = currentTurn === "W" ? "B" : "W";
+}
+
+
+/**
  * Creates a chess board DOM element and appends it to the chess-container
  */
 function generateBoard() {
@@ -76,162 +236,4 @@ function generatePieces() {
         pieceImage.classList.add("chess-icon")
         square.appendChild(pieceImage)
     }
-}
-
-
-/**
- * Allows data/elements to be dropped in other elements
- * @param {Event} ev 
- */
-function allowDrop(ev) {
-    ev.preventDefault()
-}
-
-let prevPieces = []
-/**
- * Modifies the chess board to show all available moves for a given piece
- * @param {Event} ev 
- */
-function displayMoves(ev) {
-    let piece = ev.target
-    if (currentTurn === piece.dataset.color) {
-        let possibleMoves = getPossibleMoves(piece)
-        possibleMoves.forEach(
-            pos => findSquare(pos[0], pos[1]).classList.add("active-square")
-        )
-        prevPieces = possibleMoves
-    }
-}
-
-
-/**
- * Clears all available moves for a given piece
- */
-function clearMoves() {
-    prevPieces.forEach(
-        pos => findSquare(pos[0], pos[1]).classList.remove("active-square")
-    )
-}
-
-
-/**
- * Stores the dragged piece position in dataTransfer (storage method for drag and drop API)
- * @param {Event} ev
- */
-function drag(ev) {
-    // The parent of the dragged chess icon is the chess square
-    let square = ev.target.parentNode
-    let row = square.dataset.row
-    let col = square.dataset.col
-
-    // Shows move guidance on drag of piece
-    displayMoves(ev)
-    
-    // Stores the position in a string where first digit is row and second digit is col
-    ev.dataTransfer.setData("pos", `${row}${col}`)
-}
-
-
-/**
- * Returns true if the move is valid; otherwise false. Valid moves are:
- *  - Correct turn
- *  - In possible moveset
- *  - Inbounds
- * @param {Node} piece
- * @param {Node} droppedSquare
- * @param {Array} currentPos
- * @returns {Boolean} 
- */
-function isValidMove(piece, droppedSquare) {
-    function deepIncludes(array, searchElement) {
-        for (const element of searchElement) {
-            if (array.length !== element.length) { 
-                return false 
-            }
-
-            let same = true
-            for (let i = 0; i < element.length; i++) {
-                if (!(array[i] === element[i])) {
-                    same = false
-                }
-            }
-            if (same) {
-                return true
-            }
-        }
-
-        return false
-    }
-
-    // Check if the piece being moved matches the current type
-    if (piece.dataset.color !== currentTurn) { return false }
-
-    let pos = getPos(droppedSquare)
-    console.log(pos, getPossibleMoves(piece))
-    if (!deepIncludes(pos, getPossibleMoves(piece))) { return false }
-
-    let hasMoved = piece.dataset.notMoved === "true" ? true : false
-    console.log(hasMoved)
-    if (piece.dataset.rank === "P") {
-        if (hasMoved) {
-            piece.dataset.notMoved = false
-        }
-    }
-
-    return true
-
-}
-
-
-/**
- * Retrieves the dragged chess piece and its position
- * @param {Event} ev
- * @returns {Array} [piece, [row, col]]
- */
-function getDraggedPiece(ev) {
-    // Get the piece that was dragged
-    let [row, col] = ev.dataTransfer.getData("pos").split("")
-    let square = findSquare(parseInt(row), parseInt(col))
-    let piece = square.firstChild
-    return piece
-}
-
-
-/**
- * When the dragged data is dropped, the chess piece element is moved to the according square
- * @param {Event} ev
- */
-function drop(ev) {
-    // Prevent default behavior of open as link on drop
-    ev.preventDefault()
-
-    // Clears piece move guidance
-    clearMoves()
-
-    // Get the square element dropped onto
-    let square = ev.target
-
-    // Prevents square from being the chess piece element
-    // (doesn't have row and col) dataset info
-    if (square.tagName === "IMG") {
-        square = ev.target.parentNode
-    }
-
-    // Get the piece that was dragged
-    let piece = getDraggedPiece(ev)
-
-    // Returns true if valid move, otherwise false
-    if (!isValidMove(piece, square)) { return }
-
-    // If there is a piece on the position, capture it
-    let opponentPiece = square.firstChild
-    if (opponentPiece) {
-        square.removeChild(opponentPiece)
-    }
-
-    // Place it on the new chess square
-    square.appendChild(piece)
-
-    // Switch turns
-    currentTurn = currentTurn === "W" ? "B" : "W";
 }
